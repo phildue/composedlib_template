83e7ce587e4c95c71de2bc05baf34d733d13e7d3
phild <phild@protonmail.com>
2020-10-11 09:13:39 +0200

    shutil

--- tools/setup_package.py
+++ tools/setup_package.py
@@ -1,27 +1,23 @@
 import os
-
+import shutil
 package_name = os.path.basename(os.getcwd())
 
-with open("CMakeLists.txt", "r") as fin:
-    with open("CMakeLists.txt", "w") as fout:
-        for line in fin:
-            fout.write(line.replace('MyLibrary', package_name))
 
-with open("test/test_mylibrary.cpp", "r") as fin:
-    with open("test/test_mylibrary.cpp", "w") as fout:
-        for line in fin:
-            fout.write(line.replace('MyLibrary', package_name))
-            fout.write(line.replace('MY_LIBRARY', package_name))
+def replace_in_file(filename):
+    with open(filename, "r") as fin:
+        with open(filename + ".tmp", "w") as fout:
+            for line in fin:
+                fout.write(line.replace('MyLibrary', package_name))
+    os.remove(filename)
+    os.rename(filename + ".tmp", filename)
 
-with open("src/MyLibrary/MyLibrary.h", "r") as fin:
-    with open("src/MyLibrary/MyLibrary.h", "w") as fout:
-        for line in fin:
-            fout.write(line.replace('MyLibrary', package_name))
-            fout.write(line.replace('MY_LIBRARY', package_name))
+
+replace_in_file("CMakeLists.txt")
+replace_in_file("test/test_mylibrary.cpp")
+replace_in_file("src/MyLibrary/MyLibrary.h")
 
 
 os.rename(r'tools/MyLibraryConfig.cmake', r'tools/' + package_name + 'Config.cmake')
 os.rename(r'src/MyLibrary/MyLibrary.h', r'src/' + package_name + ".h")
-os.rename(r'src/MyLibrary', r'src/' + package_name)
+shutil.move(r'src/MyLibrary', r'src/' + package_name)
 os.rename(r'test/test_mylibrary.cpp', r'test/test_' + package_name + ".cpp")
-
