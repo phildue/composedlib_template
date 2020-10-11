a3dfae40254182420dd5982a81b9b615e5620bb6
phild <phild@protonmail.com>
2020-10-11 09:16:20 +0200

    also replacing in test/CMakelists

--- tools/setup_package.py
+++ tools/setup_package.py
@@ -13,6 +13,8 @@ def replace_in_file(filename):
 
 
 replace_in_file("CMakeLists.txt")
+replace_in_file("test/CMakeLists.txt")
+
 replace_in_file("test/test_mylibrary.cpp")
 replace_in_file("src/MyLibrary/MyLibrary.h")
 
